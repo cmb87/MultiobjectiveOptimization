eb8b97cf66737d5ecf5390fbe036edbcdb2290e9
cmb87 <cmb87>
2019-08-04 16:27:31 +0200

    added cross validation for GP

--- src/gaussianprocess.py
+++ src/gaussianprocess.py
@@ -4,69 +4,119 @@ import numpy as np
 
 class GaussianProcess(object):
     ### Constructor ###
-    def __init__(self, xtrain, ytrain, param=[0.3, 0.0]):
+    def __init__(self, xtrain, ytrain, param=None, noise=0.0):
         self.xtrain = xtrain
         self.ytrain = ytrain
-        self.param = param
-        self.noise = 1e-5
+        self.param = 0.3*np.ones(xtrain.shape[1]) if param is None else np.asarray(param)
+        self.noiseParam = noise
+
 
     ### RBF Kernel ###
     @staticmethod
-    def kernel(a, b, param):
-
+    def kernel(a, b, param, noisevar):
+        ### Noise ####
         c = np.zeros((a.shape[0], b.shape[0]))
-        np.fill_diagonal(c, param[1]**2)
+        np.fill_diagonal(c, noisevar**2)
+        ### Kernel fct ###
+        a=a/param
+        b=b/param
         sqdist = np.sum(a**2,1).reshape(-1,1) + np.sum(b**2,1) - 2*np.dot(a, b.T)
-        return np.exp(-.5 * (1/param[0]) * sqdist) + c
+        return np.exp(-.5 * sqdist) + c
+
+        #sqdist = np.sum(a**2,1).reshape(-1,1) + np.sum(b**2,1) - 2*np.dot(a, b.T)
+        #return np.exp(-.5 * (1/param[0]) * sqdist) + c
 
     ### Predict new values ###
     def predict(self, xtest, param=None):
 
         param = self.param if param is None else param
 
-        K_ss = GaussianProcess.kernel(xtest, xtest, param)
-        K_s  = GaussianProcess.kernel(self.xtrain, xtest, param)
-        K    = GaussianProcess.kernel(self.xtrain, self.xtrain, param)
+        K_ss = GaussianProcess.kernel(xtest, xtest, param, self.noiseParam)
+        K_s  = GaussianProcess.kernel(self.xtrain, xtest, param, self.noiseParam)
+        K    = GaussianProcess.kernel(self.xtrain, self.xtrain, param, self.noiseParam)
 
-        L = np.linalg.cholesky(K + self.noise*np.eye(self.xtrain.shape[0]))
+        L = np.linalg.cholesky(K + 1e-5*np.eye(self.xtrain.shape[0]))
         Lk = np.linalg.solve(L, K_s)
 
         z = np.linalg.solve(L, self.ytrain)
+
         mu = np.dot(Lk.T, z).reshape((-1,))
-        var = np.linalg.cholesky(K_ss + self.noise*np.eye(xtest.shape[0]) - np.dot(Lk.T, Lk))
+        var = np.linalg.cholesky(K_ss + 1e-5*np.eye(xtest.shape[0]) - np.dot(Lk.T, Lk))
+
+        # Compute the standard deviation so we can plot it
+        s2 = np.diag(K_ss) - np.sum(Lk**2, axis=0)
 
         ### P value ###
-        logP = -0.5*np.dot(z.T,z).reshape(1) - np.trace(np.log(L)).reshape(1)
+        logP = -0.5*np.dot(z.T,z).reshape(1) - np.trace(np.log(np.abs(L)+1e-10)).reshape(1)
 
-        return mu, var, logP[0]
+        return mu, var, s2**0.5, logP[0]
+
+    ### cross-validated ###
+    def crossvalidated(self, p0=None, kfold=4, **args):
+        
+        p0 = self.param if p0 is None else p0
+        size = int(self.xtrain.shape[0]/kfold)
+
+        self.xtrain0 = self.xtrain.copy()
+        self.ytrain0 = self.ytrain.copy()
+        p_opti = []
+
+        print("Starting crossvalidation")
+        for k in range(kfold):
+            index = np.ones(self.xtrain0.shape[0], dtype=bool)
+            index[k*size:(k+1)*size] = False
+
+            self.xtrain = self.xtrain0[index,:]
+            self.ytrain = self.ytrain0[index,:]
+            xtest = self.xtrain0[~index,:]
+            ytest = self.ytrain0[~index,:]
+
+            ### Tune system ###
+            p_opti.append(self.tuneParameters(xtest, p0, **args))
+
+            ### Calc Error ###
+            yhat,_,_,_ = self.predict(xtest)
+            rms = np.mean((yhat-ytest)**2)
+            print("-> kfold: {}/{}, train/test: {}/{}, RMSE: {:.5}".format(k+1, kfold,self.xtrain.shape[0] ,xtest.shape[0] , rms))
+
+        p_opti = np.asarray(p_opti)
+        self.param = np.mean(p_opti, axis=0)
+        self.xtrain = self.xtrain0
+        self.ytrain = self.ytrain0
+        del self.xtrain0, self.ytrain0
+
 
     ### Hyperparameter fitting with GD ###
-    def tuneParameters(self, xtest, p0, max_iters=2000, learn_rate=0.01, delta=0.001, eps=1e-4, plot=False):
+    def tuneParameters(self, xtest, p0, max_iters=2000, learn_rate=0.01, delta=0.001, eps=1e-4, plot=False, verbose=False):
         p = np.asarray(p0).reshape(-1)
         logPs = []
         for i in range(max_iters):
-            grady = np.zeros_like(p)
 
+            ### Calc gradient ###
+            grady = np.zeros_like(p)
             for i in range(p.shape[0]):
                 dx = np.zeros_like(p)
                 dx[i] = delta
-                _, _, yf = self.predict(xtest, param=p+dx)
-                _, _, yb = self.predict(xtest, param=p-dx)
+                _, _, _, yf = self.predict(xtest, param=p+dx)
+                _, _, _, yb = self.predict(xtest, param=p-dx)
                 grady[i] = (yf-yb)/(2*delta)
 
             step = (learn_rate*grady).reshape(-1)
             p += step
 
             ### Get current value ###
-            _, _, y = self.predict(xtest, param=p)
+            _, _, _, y = self.predict(xtest, param=p)
             logPs.append(y)
-            print("new logp: {}, p: {}".format(y, p))
+            if verbose:
+                print("new logp: {}, p: {}".format(y, p))
 
             if np.all(step<eps):
-                print("No further improvement observed!")
+                if verbose:
+                    print("No further improvement observed!")
                 break
             elif i == max_iters-1:
-                print("Maximum iteration number reached!")
+                if verbose:
+                    print("Maximum iteration number reached!")
 
         ### Assign optimial parameters to param ###
         self.param = p
@@ -78,6 +128,10 @@ class GaussianProcess(object):
             plt.xlabel("Iterations")
             plt.ylabel("log P(y|x,p)")
             plt.show()
+
+        ### Return optimized parameters ###
+        return p
+
 
 
 
@@ -91,27 +145,31 @@ if __name__ == "__main__":
         ytrain = np.sin(Xtrain)
         Xtest = np.linspace(-5, 5, 50).reshape(-1,1)
 
-        gp = GaussianProcess(Xtrain, ytrain, param=[0.8,0.5])
+        gp = GaussianProcess(Xtrain, ytrain, param=[0.8], noise=0.0)
         #gp.tuneParameters(Xtest, p0=[0.8,0.1], plot=True, eps=0.001)
-        YtestHat, Yvar, logP = gp.predict(Xtest)
 
-        print(Yvar.shape)
+        gp.crossvalidated(kfold=5, plot=False, eps=0.0001, verbose=False)
 
+        YtestHat, Yvar, sigma, logP = gp.predict(Xtest)
+        
+
+        [plt.plot(Xtest, YtestHat.reshape(-1,1) + np.dot(Yvar,np.random.normal(size=(50,1)))) for _ in range(10)]
+
+        
+        plt.gca().fill_between(Xtest.flat, YtestHat-3*sigma, YtestHat+3*sigma, color="#dddddd")
+        plt.plot(Xtest, YtestHat, 'r--', lw=5)
         plt.plot(Xtrain, ytrain, 'bs', ms=8)
-       # plt.gca().fill_between(Xtest.flat, YtestHat-2*Yvar, YtestHat+2*Yvar, color="#dddddd")
-        plt.plot(Xtest, YtestHat, 'r--', lw=2)
         plt.axis([-5, 5, -3, 3])
         plt.grid(True)
         plt.show()
 
-    if False:
+    if True:
         np.random.seed(42)
         ### 2D Test problem ###
         def rosenbrock(X):
             return (1-X[:,0])**2 + 100*(X[:,1]-X[:,0]**2)**2
 
-        
-        Xtrain = np.asarray([-2,-1]) + np.random.rand(50, 2)*(np.asarray([2,3])-np.asarray([-2,-1]))
+        Xtrain = np.asarray([-2,-1]) + np.random.rand(10, 2)*(np.asarray([2,3])-np.asarray([-2,-1]))
         Ytrain = rosenbrock(Xtrain).reshape(-1,1)
 
         XG,YG = np.meshgrid(np.linspace(-2,2,20),np.linspace(-1,3,20))
@@ -123,20 +181,21 @@ if __name__ == "__main__":
         Ytrain = (Ytrain-zmin)/(zmax-zmin)
 
         ### Initialiaze GP ###
-        gp = GaussianProcess(Xtrain, Ytrain, 0.8)
-        gp.tuneParameters(Xtest, p0=[0.8,1.0], plot=True, eps=0.01)
-        YtestHat, Yvar, logP = gp.predict(Xtest)
+        gp = GaussianProcess(Xtrain, Ytrain, noise=0.00)
+        gp.crossvalidated(kfold=2, plot=False, eps=0.0001, verbose=False)
+        #gp.tuneParameters(Xtest, p0=[0.3,0.3], plot=True, eps=0.0001, verbose=True)
 
+        YtestHat, Yvar, sigma, logP = gp.predict(Xtest)
 
+        print(gp.param)
         ### The plotting ###
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         ax.plot_wireframe(XG, YG, ZG, color='k')
-        ax.plot(Xtrain[:,0], Xtrain[:,1], Ytrain[:,0],'ro', markersize=12)
+        
         ax.plot_wireframe(Xtest[:,0].reshape(20,20), Xtest[:,1].reshape(20,20), YtestHat.reshape(20,20), color='r')
+        ax.plot_wireframe(Xtest[:,0].reshape(20,20), Xtest[:,1].reshape(20,20), YtestHat.reshape(20,20)+3*sigma.reshape(20,20), color='gray')
+        ax.plot_wireframe(Xtest[:,0].reshape(20,20), Xtest[:,1].reshape(20,20), YtestHat.reshape(20,20)-3*sigma.reshape(20,20), color='gray')
 
-
-        #for n, color in zip(range(NDRAWS),('g','m', 'b')):
-        #    ax.plot_wireframe(Xtest[:,0].reshape(20,20), Xtest[:,1].reshape(20,20), f_post[:,n].reshape(20,20), color=color)
-
+        ax.plot(Xtrain[:,0], Xtrain[:,1], Ytrain[:,0],'ro', markersize=12)
         plt.show()
