228c64df267fe67c10c5e35c4c199b826939a8f7
cmb87 <cmb87>
2019-08-25 18:01:24 +0200

    added recurremt neat

--- src/neat/innomanager.py
+++ src/neat/innomanager.py
@@ -1,29 +1,41 @@
 
 
-
-
-
 class InnovationManager:
     def __init__(self):
-        self.innovations = {}
-        self.seenInnovations = {}
+        self.innovationArchive = {}
         self.innovationCtr = 0
+        self.nodectr = 0
 
     ### Add innovation ###
-    def add(self, n1, n2, generation=None):
+    def add(self, n1, n2, level, generation=None):
+        ### name of the invention ###
+        name = '{}-{}-{}'.format(level, n1, n2)
+        ### Get highest node id ###
+        self.nodectr = max([n1 ,n2, self.nodectr])
+
         ### I have already seen this one ###
-        if '{}-{}'.format(n1, n2) in list(self.seenInnovations.keys()):
-            return self.seenInnovations['{}-{}'.format(n1, n2)]
+        if name in list(self.innovationArchive.keys()):
+            return self.innovationArchive[name]["innovationNumber"]
 
         ### This combination doesn't exist yet, adding it ... ###
         self.innovationCtr+=1
-        self.seenInnovations['{}-{}'.format(n1, n2)] = self.innovationCtr
-        self.innovations[self.innovationCtr] = {"nodes": [n1, n2], "generation": generation}
-        print('New innovation ({}) {}-{} added!'.format(self.innovationCtr, n1, n2))
+        self.innovationArchive[name] = {"nodes": [n1, n2], "generation": generation, "level": level,
+                                        "innovationNumber": self.innovationCtr}
+
+        print('New innovation ({}) {} added!'.format(self.innovationCtr, name))
         return self.innovationCtr
 
+    ### for mutate add node ###
+    def getNewNodeID(self):
+        self.nodectr+=1
+        print("New node ({}) added!".format(self.nodectr))
+        return self.nodectr
 
-    ### return nids and connection ###
-    def getFeatureFromIno(self, inos):
+    ### Return nodes ###
+    def getNodes(self, ino):
+        return [val for key, val in self.innovationArchive.items() if val["innovationNumber"] == ino][0]
 
-        nids = list(set([n for n in self.innovations[ino]["nodes"] for ino in inos]))
+
+
+
+        
--- src/neat/species.py
+++ src/neat/species.py
@@ -1,7 +1,10 @@
 import numpy as np
 import json
+import re
+import copy
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
 
 from activations import ACTIVATIONS
 from innomanager import InnovationManager
@@ -11,22 +14,17 @@ from innomanager import InnovationManager
 class Specie:
 
     ### Constructor ###
-    def __init__(self, nids, nfcts, connections, nids_input, nids_output, recurrent_connections=None, bias=None, _id=None,
-                 generation=0, parents=[None,None], crossover=True, iter_survived=0, innovations=[], innovationManager=None):
+    def __init__(self, nids, nids_input, nids_output, structure, maxtimelevel=2,
+                 _id=None, generation=0, parents=[None,None], crossover=True, iter_survived=0):
 
         ### Structural parameters ###
         self.nids = nids
-        self.nfcts = nfcts
-        self.bias = len(nids)*[0] if bias is None else bias
-        self.connections = connections
-        self.recurrent_connections = len(nids)*[[[],[]]] if recurrent_connections is None else recurrent_connections
-        
-        assert len(self.bias)  == len(self.nids ), "Length of bias must match length of Nids!"
-        assert len(self.nfcts) == len(self.nids ), "Length of nodal functions must match length of Nids!"
-        assert len(self.connections) == len(self.nids ), "Length of connections must match length of Nids!"
-        assert len(self.recurrent_connections) == len(self.nids ), "Length of recurrent connections must match length of Nids!"
+        self.structure = structure
+        self.maxtimelevel = maxtimelevel
+
+        assert len(self.structure)  == len(self.nids), "Length of structure must match length of Nids!"
         ### Previous nodestate ###
-        self.node_states0 = None
+        self.last_states = []
 
         ### For prediction ###
         self.nids_input  = nids_input
@@ -40,18 +38,15 @@ class Specie:
         self.parents = parents
         self.crossover = crossover
         self.iter_survived = iter_survived
-        self.innovations = innovations
-        ### I'll be specified outside ###
-        self.innovationManager = innovationManager
+
 
     ### to Json ###
     def json(self):
         ### Note that the innvation manager is explicitly not added here! ###
-        return {"_id": self._id, "nids": self.nids, "nfcts": self.nfcts, "connections": self.connections, 
-                "bias": self.bias, "recurrent_connections": self.recurrent_connections,
+        return {"_id": self._id, "nids": self.nids, "structure": self.structure, "maxtimelevel":self.maxtimelevel,
                 "nids_input": self.nids_input, "nids_output": self.nids_output, "generation": self.generation,
                 "parents": self.parents, "crossover": self.crossover, "iter_survived": self.iter_survived,
-                "innovations": self.innovations}
+                }
 
     ### print detailed structure ###
     def show(self):
@@ -63,31 +58,15 @@ class Specie:
 
     ### Check for innovations ###
     def checkInnovations(self):
-        self.innovations = []
-        if self.innovationManager is None:
-            print("No InnovationManager specified!")
-            return
-
-        ### Innovation manager is specified ###  
-        for nid_index, nid in enumerate(self.nids):
-            snids,  weights = self.connections[nid_index]
-            for i, snid in enumerate(snids):
-                self.innovations.append(self.innovationManager.add(snid, nid))
-
-
-    ### Use networkx to create a graph ###
-    def showGraph(self):
-        G=nx.DiGraph(specie=self._id)
-        G.add_nodes_from(self.nids, fcts=self.nfcts, bias=self.bias)
-        for nid_index, nid in enumerate(self.nids):
-            snids,  weights = self.connections[nid_index]
-            for i, snid in enumerate(snids):
-                G.add_edge(snid, nid)
-                G[snid][nid]["weight"] = weights[i]
-
-        nx.draw(G, with_labels=True)
-        plt.show()
-
+        if True:
+            ### Innovation manager is specified ###  
+            for nid in self.nids:
+                self.structure[nid]["connections"]["innovations"] = []
+                snids, levels = self.structure[nid]["connections"]["snids"], self.structure[nid]["connections"]["level"]
+                for snid, level in zip(snids, levels):
+                    self.structure[nid]["connections"]["innovations"].append(innovationManager.add(snid, nid, level))
+        else:
+            print("Global innovationManger is not defined!")
 
 
     ### Make a forward prediction ###
@@ -97,120 +76,220 @@ class Specie:
         assert X.shape[1] == len(self.nids_input_index), "Input nodes must match dimesion of X!"
 
         ### Initialize node states ###
-        node_states = np.zeros((X.shape[0], len(self.nids)))
+        node_states = np.zeros((X.shape[0], len(self.nids), self.maxtimelevel+1))
 
         ### Initialiaze previous node state for recurrent connections ###
-        self.node_states0 = node_states.copy() if self.node_states0 is None else self.node_states0
+        if len(self.last_states)>0:
+            for t, last_state in enumerate(self.last_states):
+                node_states[:,:,t+1] = last_state
 
         ### Assign values to input nodes ###
-        node_states[:,self.nids_input_index] = X
+        node_states[:,self.nids_input_index, 0] = X
 
         ### Forward propagation ###
         for nid_index, nid in enumerate(self.nids):
 
-            snids,  weights = self.connections[nid_index]
-            snids0, weights0 = self.recurrent_connections[nid_index]
-            snids_index  = [self.nids.index(snid) for snid in snids]
-            snids_index0 = [self.nids.index(snid) for snid in snids0]
+            snids  = self.structure[nid]["connections"]["snids"]
+            weights  = self.structure[nid]["connections"]["weights"]
+            level = self.structure[nid]["connections"]["level"] # The time level of the connection
+            bias = self.structure[nid]["bias"]
+            fct = ACTIVATIONS[self.structure[nid]["activation"]]
 
-            afct = ACTIVATIONS[self.nfcts[nid_index]]
+            snids_index = [self.nids.index(snid) for snid in snids]
 
             ### Calculate node state value ###
             if len(snids) > 0:
-                assert nid_index>max(snids_index), "Network is not feedforward!"
-                node_states[:,nid_index] += afct(  np.sum(np.asarray(weights )*      node_states[:, snids_index ], axis=1) + \
-                                                 + np.sum(np.asarray(weights0)*self.node_states0[:, snids_index0], axis=1) + \
-                                                 + self.bias[nid_index])
+                assert nid_index>max([snid for l, snid in zip(level,snids_index) if l == 0]), "Network is not feedforward!"
+                node_states[:,nid_index, 0] += fct(np.sum(np.asarray(weights)* node_states[:, snids_index, level], axis=1) + bias)
+
+            ### The node seems not to have any input ###
             else:
                 if not nid_index in self.nids_input_index:
                     print("Warning: Node {} seems not to be used".format(nid))
                 continue
 
         ### Store node state (for recurrent connections) ###
-        self.node_states0 = node_states.copy()
+        self.last_states.insert(0, node_states[:,:,0].copy())
+        self.last_states = self.last_states[:self.maxtimelevel]
 
-        return node_states[:,self.nids_output_index].reshape(X.shape[0],-1)
+        ### Return output nodes values ###
+        return node_states[:,self.nids_output_index, 0].reshape(X.shape[0],len(self.nids_output_index))
+
+    ### Remove node ###
+    @classmethod
+    def mutate_remove_node(cls, specie1, generation=None):
+        nids = specie1.nids.copy()
+        structure = copy.deepcopy(specie1.structure)
+        nid_remove = nids[np.random.randint(len(specie1.nids_input), len(nids)-len(specie1.nids_output))]
+    
+        ### Remove node ###
+        nids.remove(nid_remove)
+        del structure[nid_remove]
+
+        ### Remove connections ###
+        for nid in nids:
+            if nid_remove in structure[nid]["connections"]["snids"]:
+                index = structure[nid]["connections"]["snids"].index(nid_remove)
+                structure[nid]["connections"]["innovations"].pop(index)
+                structure[nid]["connections"]["weights"].pop(index)
+                structure[nid]["connections"]["snids"].pop(index)
+                structure[nid]["connections"]["level"].pop(index)
+
+        print("Node {} removed".format(nid_remove))
+        return cls(nids=nids, structure=structure, nids_output=specie1.nids_output, nids_input=specie1.nids_input,
+                   maxtimelevel=specie1.maxtimelevel, generation=generation, parents=[specie1._id])
+
+    ### Add node ###
+    @classmethod
+    def mutate_add_node(cls, specie1, value_in=0.1, value_out=0.1, generation=None):
+
+        nids = specie1.nids.copy()
+        structure = copy.deepcopy(specie1.structure)
+        nid_new = innovationManager.getNewNodeID()
+
+        inos = {ino:nid for nid in specie1.nids for ino in specie1.structure[nid]["connections"]["innovations"]}
+        inomut = list(inos.keys())[np.random.randint(0, len(inos))]
+
+        innovation = innovationManager.getNodes(inomut)
+        [snid, rnid], level = innovation["nodes"], innovation["level"]
+
+        ### Inserting new point ###
+        for nid_index, nid in enumerate(nids):
+            if nid_index <= nids.index(snid):
+                continue
+            if nid_index > nids.index(rnid):
+                continue
+            index_insert = nid_index
+            break
+
+        nids.insert(index_insert, nid_new)
+
+        ### Delete old connection ###
+        index = structure[inos[inomut]]["connections"]["innovations"].index(inomut)
+        structure[inos[inomut]]["connections"]["innovations"].pop(index)
+        structure[inos[inomut]]["connections"]["weights"].pop(index)
+        structure[inos[inomut]]["connections"]["snids"].pop(index)
+        structure[inos[inomut]]["connections"]["level"].pop(index)
+
+        ### Add connection from new node ###
+        structure[inos[inomut]]["connections"]["innovations"].append(innovationManager.add(nid_new, rnid, level))
+        structure[inos[inomut]]["connections"]["weights"].append(value_out)
+        structure[inos[inomut]]["connections"]["snids"].append(nid_new)
+        structure[inos[inomut]]["connections"]["level"].append(level)
+
+        ### Add connection to new node ###
+        structure[nid_new] = {"connections": {}}
+        structure[nid_new]["connections"]["innovations"] = [innovationManager.add(snid, nid_new, level)]
+        structure[nid_new]["connections"]["weights"] = [value_in]
+        structure[nid_new]["connections"]["snids"] = [snid]
+        structure[nid_new]["connections"]["level"] = [level]
+        structure[nid_new]["bias"] = 0.0
+        structure[nid_new]["activation"] = np.random.randint(0,len(ACTIVATIONS))
+
+
+
+        return cls(nids=nids, structure=structure, nids_output=specie1.nids_output, nids_input=specie1.nids_input,
+                   maxtimelevel=specie1.maxtimelevel, generation=generation, parents=[specie1._id])
 
 
     ### Crossover specie 1 is assumed to be the fitter one ###
     @classmethod
-    def crossover(cls, specie1, specie2):
-
-        complete = list(set(specie1.innovations+specie2.innovations))
-        common = list(set(specie1.innovations) & set(specie2.innovations))
-        uncom1 = list(set(specie1.innovations).difference(set(common)))
-        uncom2 = list(set(specie2.innovations).difference(set(common)))
-
-        print("Crossover")
-
-        inos = len(complete)*[None]
-        ### Common numbers ###
-        common_index = [max([specie1.innovations.index(ino), specie2.innovations.index(ino)]) for ino in common]
-        for index, ino in zip(common_index, common):
-            inos[index] = ino
+    def crossover(cls, specie1, specie2, generation=None):
         
-        disjoint, excess = [], []
-        ### Uncommon numbers ###
-        for n, ino in enumerate(complete):
-            if ino in uncom1:
-                s1_index = specie1.innovations.index(ino)
-                inos[s1_index] = ino
-                if s1_index > max(common_index):
-                    excess.append(ino)
-                else:
-                    disjoint.append(ino)
+        inos1 = {ino:nid for nid in specie1.nids for ino in specie1.structure[nid]["connections"]["innovations"]}
+        inos2 = {ino:nid for nid in specie2.nids for ino in specie2.structure[nid]["connections"]["innovations"]}
 
-            elif ino in uncom2:
-                s2_index = specie2.innovations.index(ino)
-                inos[s2_index] = ino
-                if s2_index > max(common_index):
-                    excess.append(ino)
-                else:
-                    disjoint.append(ino)
+        cinos  = list(set(inos1.keys()) & set(inos2.keys()))
+        uinos1 = list(set(inos1.keys()).difference(set(cinos)))
+
+        nids = specie1.nids.copy()
+
+        structure = {nid: {'connections':{"snids":[], "weights":[], "level":[]}, 
+                           'activation': None, 'bias': None} for nid in nids}
+
+        ### inputs ###
+        for nid in specie1.nids_input:
+            structure[nid] = specie1.structure[nid]
+
+        ### Common innovations ###
+        for cino in cinos:
+            parent = [specie1.structure[inos1[cino]], specie2.structure[inos2[cino]]][np.random.randint(0,2)]
+            
+            innovation = innovationManager.getNodes(cino)
+            snid, level = innovation["nodes"][0], innovation["level"]
+
+            weight = parent["connections"]['weights'][parent["connections"]['snids'].index(snid)]
+
+            structure[inos1[cino]]['connections']['snids'].append(snid)
+            structure[inos1[cino]]['connections']['level'].append(level)
+            structure[inos1[cino]]['connections']['weights'].append(weight)
+            structure[inos1[cino]]['bias'] = parent["bias"]
+            structure[inos1[cino]]['activation'] = parent["activation"]
+
+        ### Uncommon (only taken from fitter species 1) ###
+        for uino in uinos1:
+            structure[inos1[uino]] = specie1.structure[inos1[uino]]
 
 
-        print(inos)
-        print(disjoint)
-        print(excess)
+        return cls(nids=nids, structure=structure, nids_output=specie1.nids_output, nids_input=specie1.nids_input,
+                   maxtimelevel=specie1.maxtimelevel, generation=generation, parents=[specie1._id, specie2._id])
+
+
+
+    ### Use networkx to create a graph ###
+    def showGraph(self):
+        G=nx.DiGraph(specie=self._id)
+        G.add_nodes_from(self.nids)
+        for nid_index, nid in enumerate(self.nids):
+            snids = self.structure[nid]["connections"]["snids"]
+            weights = self.structure[nid]["connections"]["weights"]
+            for i, snid in enumerate(snids):
+                G.add_edge(snid, nid)
+                G[snid][nid]["weight"] = weights[i]
+
+        nx.draw(G, with_labels=True)
+        plt.show()
+
 
 #### TEST #######
 if __name__ == "__main__":
 
 
-    innomanager = InnovationManager()
+    innovationManager = InnovationManager()
 
-    connections = [ [[],[]],
-                    [[],[]],
-                    [[1,2],[1.0, 1.0]],
-                    [[1,2],[1.0, 1.0]],
-                    [[3,5],[1.0, 1.0]],
-                    ]
+    structure = {1: {"connections": {"snids": [], "weights":[],"innovations":[], "level":[]},  "activation": 0, "bias":0},
+                 2: {"connections": {"snids": [], "weights":[],"innovations":[], "level":[]},  "activation": 0, "bias":0},
+                 3: {"connections": {"snids": [1,2], "weights":[1.0, 1.0], "innovations":[], "level":[0,0]},  "activation": 1, "bias":0},
+                 5: {"connections": {"snids": [1,2], "weights":[2.0, 1.0], "innovations":[], "level":[0,0]},  "activation": 1, "bias":0},
+                 6: {"connections": {"snids": [3,5], "weights":[1.0, 1.0], "innovations":[], "level":[0,0]},  "activation": 0, "bias":0},
+                }
 
-    specie1 = Specie(nids=[1,2,3,5,6], nfcts=[0,0,1,1,0], connections=connections, recurrent_connections=None,
-                    nids_input=[1,2], nids_output=[6], innovationManager=innomanager)
-
-
-    connections = [ [[],[]],
-                    [[],[]],
-                    [[1,2],[1.0, 1.0]],
-                    [[3,2],[1.0, 1.0]],
-                    [[1,2],[1.0, 1.0]],
-                    [[3,5,7],[1.0, 1.0, 1.0]],
-                    ]
-
-    specie2 = Specie(nids=[1,2,3,7,5,6], nfcts=[0,0,1,1,1,0], connections=connections, recurrent_connections=None,
-                    nids_input=[1,2], nids_output=[6], innovationManager=innomanager)
-
-
+    specie1 = Specie(nids=[1,2,3,5,6], structure=structure, nids_input=[1, 2], nids_output=[6])
     specie1.checkInnovations()
+
+
+    structure = {1: {"connections": {"snids": [], "weights":[],"innovations":[], "level":[]},  "activation": 0, "bias":0},
+                 2: {"connections": {"snids": [], "weights":[],"innovations":[], "level":[]},  "activation": 0, "bias":0},
+                 5: {"connections": {"snids": [1,2], "weights":[2.0, 1.0], "innovations":[], "level":[0,0]},  "activation": 1, "bias":0},
+                 7: {"connections": {"snids": [1,5], "weights":[2.0, 1.0], "innovations":[], "level":[0,0]},  "activation": 1, "bias":0},
+                 6: {"connections": {"snids": [5,7], "weights":[1.0, 1.0], "innovations":[], "level":[0,0]},  "activation": 0, "bias":0},
+                }
+
+    specie2 = Specie(nids=[1,2,5,7,6], structure=structure, nids_input=[1, 2], nids_output=[6])
     specie2.checkInnovations()
 
-    print(specie1.innovations)
-    print(specie2.innovations)
-    Specie.crossover(specie1, specie2)
 
-    #specie2.showGraph()
+    specie3 = Specie.crossover(specie1, specie2)
+    specie4 = Specie.mutate_add_node(specie1)
+    specie5 = Specie.mutate_remove_node(specie4)
+    specie6 = Specie.crossover(specie4, specie5)
+    
+    specie3.showGraph()
+    specie5.showGraph()
+    # 
 
-    X = np.random.rand(10,2)
-    y = specie1.run(X)
-
+    for _ in range(1):
+        print("####", _ ,"####")
+        X = np.random.rand(10,2)
+        y = specie3.run(X)
+        print(y)
